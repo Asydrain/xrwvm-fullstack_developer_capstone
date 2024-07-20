5723c7f2ae0161b6601500d58f8c55501322fda2
Syd Frandsen <sydevanfrandsen@gmail.com>
2024-07-20 16:44:35 -0400

    correcting lint errors in python files

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,19 +2,23 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 # CarModelInline class
+
 class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 3
 
 # CarModelAdmin class
+
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ['name']
 
 # CarMakeAdmin class with CarModelInline
+
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
     list_display = ['name']
 
-# Register models 
+# Register models
+
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
